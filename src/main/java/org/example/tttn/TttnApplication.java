5de1f43c57cbe7c098ba120b6375b8c4554ff62d
Yong Jin Su <dungto0300567@gmail.com>
2025-09-05 10:01:28 +0700

    Initial commit

---
+++ src/main/java/org/example/tttn/TttnApplication.java
@@ -0,0 +1,13 @@
+package org.example.tttn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TttnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tttn/TttnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tttn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
